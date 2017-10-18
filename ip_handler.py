9c3a708bc808514a74d6555dab848e18f744d209
mohan <mohan.kst.hp@gmail.com>
2017-10-18 14:11:56 +0600

    changing IP by HMA! pro by running windows batch file

--- ip_handler.py
+++ ip_handler.py
@@ -1,21 +1,7 @@
 import ipgetter, subprocess, time, os
 
-original_ip = '52.14.48.000'
-current_ip = ipgetter.myip()
-
-def check_ip():
-    new_current_ip = ipgetter.myip()
-    if not new_current_ip == original_ip and new_current_ip == original_ip:
-        result = True
-    else:
-        result = False
-    return result
-
+# changing IP by HMA! pro by running windows batch file
 def change_ip():
     os.system('connect.bat')
     os.system('changeip.bat')
-    time.sleep(5)
-    ip_changed = check_ip()
-    while ip_changed == False:
-        time.sleep(7)
-        ip_changed = check_ip()
+    time.sleep(40)
