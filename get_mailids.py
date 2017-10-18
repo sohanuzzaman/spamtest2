041240c9a3d6a820aa77cde202ab48885c0f1005
mohan <mohan.kst.hp@gmail.com>
2017-10-18 14:12:24 +0600

    Add leads automatically to sending list

--- get_mailids.py
+++ get_mailids.py
@@ -52,33 +52,59 @@ final_mail_subject = str(raw_mail_subject)
 
 def receiver_list():
     receivers = []
+    limit_over = "b.(550, b'5.4.5 Daily user sending quota exceeded. m69sm15876126pfk.54 - gsmtp')"
     for row in email_leads:
         lead = row['leads']
-        receivers.append(lead)
+        status = row['status']
+        #Adding pending email leads only to sending list
+        if status == "a.pending" or limit_over:
+            receivers.append(lead)
+        else:
+            break
     return receivers
 
 all_receiver = receiver_list()
 
 
 
+# Handel reporting
+def mailid_err_renew(mialid_row_index):
+    #use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    
+    sender_email_id_sheet = client.open("campaignmails").sheet1
+    sender_email_id_sheet.update_cell(mialid_row_index, 5, "Failed to login via SMTP")
+
+def lead_err_renew(lead_row_index, message):
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+
+    email_leads_sheet = client.open("leads").sheet1
+    email_leads_sheet.update_cell(lead_row_index, 2, message)
 
 
 
 # Handel reporting
 def mailid_err(mialid_row_index):
-    # # use creds to create a client to interact with the Google Drive API
-    # scope = ['https://spreadsheets.google.com/feeds']
-    # creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-    # client = gspread.authorize(creds)
-    #
-    # sender_email_id_sheet = client.open("campaignmails").sheet1
-    sender_email_id_sheet.update_cell(mialid_row_index, 5, "Failed to login via SMTP")
+    try:
+        sender_email_id_sheet.update_cell(mialid_row_index, 5, "Failed to login via SMTP")
+
+    except:
+        mailid_err_renew(mialid_row_index)
+    
 
 def lead_err(lead_row_index, message):
     # # use creds to create a client to interact with the Google Drive API
     # scope = ['https://spreadsheets.google.com/feeds']
     # creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-    # client = gspread.authorize(creds)
+    try:
+        email_leads_sheet.update_cell(lead_row_index, 2, message)
 
-    # email_leads_sheet = client.open("leads").sheet1
-    email_leads_sheet.update_cell(lead_row_index, 2, message)
+    except:
+        lead_err_renew(lead_row_index, message)
+
+
