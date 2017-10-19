190ca94c9e5c730f8ed478db732dcee42bb1f159
mohan <mohan.kst.hp@gmail.com>
2017-10-19 17:04:06 +0600

    Added automatic IP change for Linux

--- ip_handler.py
+++ ip_handler.py
@@ -1,9 +1,20 @@
 import ipgetter, subprocess, time, os
-from sys import platform
+import platform
+
+
+#Change IP function for linux
+def change_ip_linux(server):
+    subprocess.call("killall openvpn", shell=True)
+    command = "openvpn --daemon --config ./UDP/{}".format(server)
+    subprocess.call(command, shell=True)
+
+
 
 # changing IP by HMA! pro by running windows batch file
-def change_ip():
-    if platform == "win32":
+def change_ip(server):
+    if platform.system() == 'Linux':
+        change_ip_linux(server)
+    elif platform.system() == "win32":
         os.system('connect.bat')
         os.system('changeip.bat')
         time.sleep(40)
