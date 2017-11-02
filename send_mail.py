e56bd18a4063d7c12a50c584adfcbd19ca577fac
mohan <mohan.kst.hp@gmail.com>
2017-11-02 14:46:32 +0600

    Added mimetext Commented out reconnet

--- send_mail.py
+++ send_mail.py
@@ -1,5 +1,9 @@
 from get_mailids import sender_email_ids, all_receiver, get_random, mailid_err, lead_err
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import formataddr
 from random import randrange
 from ip_handler import reconnect
 from smtp_conf import connect_smtp
@@ -18,7 +22,7 @@ for item in all_receiver:
         occupation = row['occupation']
         vpn_server = row['vpn_server']
         mialid_row_index = row['index']
-        reconnect_init = reconnect(vpn_server, reconnect_init)
+        # reconnect_init = reconnect(vpn_server, reconnect_init)
         server = connect_smtp(email_id, vpn_server)
         server.ehlo()
         server.starttls()
@@ -46,11 +50,16 @@ for item in all_receiver:
                 break
             subject = get_random("subject")
             body = get_random("body")
-            message = """From: {0}\nTo: {1}\nSubject: {2}\n\n{3}\n\n{4}\n{5}
-            """.format(email_id, email_receiver, subject, body, name, occupation)
-
+            # message = """From: {0}\nTo: {1}\nSubject: {2}\n\n{3}\n\n{4}\n{5}
+            # """.format(email_id, email_receiver, subject, body, name, occupation)
+            msg = MIMEMultipart('alternative')
+            msg['From'] = formataddr((str(Header(name)), email_id))
+            msg['To'] = email_receiver
+            msg['Subject'] = subject
+            html = "{0}\n\n{1}\n{2}".format(body, name, occupation)
+            msg.attach(MIMEText(html, 'html'))
             try:
-                server.sendmail(email_id, email_receiver, message)
+                server.sendmail(email_id, email_receiver, msg.as_string())
                 lead_err(lead_row_index, "c.sent")
                 print("mail sucessfully sent to {}".format(email_receiver))
             except Exception as ex:
--- smtp_conf.py
+++ smtp_conf.py
@@ -1,5 +1,5 @@
 import re, smtplib, time
-from ip_handler import reconnect
+from ip_handler import reconnect, disconnect
 from random import choice, shuffle
 
 
@@ -28,7 +28,7 @@ def connect_smtp(email_id, vpn_server):
         server = smtplib.SMTP(smtp_server, smtp_port)
     except:
         print("failed connecting smtp server :( retrying ...")
-        reconnect(vpn_server, 0)
+        # reconnect(vpn_server, 0)
         time.sleep(10)
         connect_smtp(email_id)
     return server
